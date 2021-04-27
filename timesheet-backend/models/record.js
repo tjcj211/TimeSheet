3e7b8d916123ca6119db3440795012252530e541
Julia <jrwilkinson@quinnipiac.edu>
2021-04-27 13:58:23 -0400

    error and disabled input conditional

--- timesheet-backend/models/record.js
+++ timesheet-backend/models/record.js
@@ -1,17 +1,20 @@
 //Timothy Carta, Victoria Gorski, Julia Wilkinson
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var recordSchema = new Schema({
-	type: {
-		type: String,
-		required: true,
-		enum: ['HOMEWORK', 'EXAM PREP', 'STUDYING'],
-		default: 'HOMEWORK',
-	},
-	minutes: {
-		type: Number,
-		required: true,
-	},
+  type: {
+    type: String,
+    required: true,
+    enum: ["HOMEWORK", "EXAM PREP", "STUDYING"],
+    default: "HOMEWORK",
+  },
+  minutes: {
+    type: Number,
+    required: true,
+  },
+  studentid: {
+    type: { type: Schema.Types.ObjectId, ref: "account" },
+  },
 });
-var recordSchema = mongoose.model('records', recordSchema);
+var recordSchema = mongoose.model("records", recordSchema);
 module.exports = recordSchema;
