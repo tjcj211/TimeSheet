2fddc02a7f945010e0e924945c2711a2f678850f
Julia <jrwilkinson@quinnipiac.edu>
2021-04-26 21:14:14 -0400

    conditional rendering for student
    hide view of creating a new class

--- timesheet-frontend/src/components/ClassesTable.jsx
+++ timesheet-frontend/src/components/ClassesTable.jsx
@@ -1,67 +1,71 @@
-import React, { Component } from 'react';
-import { getProfessorClasses, saveClass } from '../service/professorService';
-import CreateClassForm from './CreateClassForm';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { getProfessorClasses, saveClass } from "../service/professorService";
+import CreateClassForm from "./CreateClassForm";
+import { Link } from "react-router-dom";
 class ClassesTable extends Component {
-	state = {
-		classes: [],
-	};
-	async componentDidMount() {
-		//TODO: add conditional for professor/student
-		const { data } = await getProfessorClasses(
-			this.props.match.params.id
-		);
-		this.setState({ classes: data });
-	}
+  state = {
+    classes: [],
+  };
+  async componentDidMount() {
+    //TODO: add conditional for professor/student
+    const { data } = await getProfessorClasses(this.props.match.params.id);
+    this.setState({ classes: data });
+  }
 
-	handleAddClass = async (class_name, class_code) => {
-		var randomClassCode =
-			Math.floor(Math.random() * 100000) +
-			'-' +
-			Math.floor(Math.random() * 100000);
-		const obj = {
-			name: class_name,
-			lesson: [],
-			class_code: randomClassCode,
-		};
-		const { data: clas } = await saveClass(
-			this.props.match.params.id,
-			obj
-		);
-		const classes = [clas, ...this.state.classes];
-		console.log(classes[0].result);
-		this.setState({ classes });
-	};
+  handleAddClass = async (class_name, class_code) => {
+    var randomClassCode =
+      Math.floor(Math.random() * 100000) +
+      "-" +
+      Math.floor(Math.random() * 100000);
+    const obj = {
+      name: class_name,
+      lesson: [],
+      class_code: randomClassCode,
+    };
+    const { data: clas } = await saveClass(this.props.match.params.id, obj);
+    const classes = [clas, ...this.state.classes];
+    console.log(classes[0].result);
+    this.setState({ classes });
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>Class Name</th>
-							<th>Class Code</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.classes.map((clas, index) => (
-							<tr key={index}>
-								<td>
-									<Link
-										to={`/${this.props.match.params.id}/classes/${clas._id}`}
-									>
-										{clas.name}
-									</Link>
-								</td>
-								<td>{clas.class_code}</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-				<CreateClassForm handleAddClass={this.handleAddClass} />
-			</React.Fragment>
-		);
-	}
+  render() {
+    let { isProfessor } = true;
+
+    const renderStudent = () => {
+      if (isProfessor) {
+        return <CreateClassForm handleAddClass={this.handleAddClass} />;
+      } else {
+        return <div> </div>;
+      }
+    };
+    return (
+      <React.Fragment>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Class Name</th>
+              <th>Class Code</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.classes.map((clas, index) => (
+              <tr key={index}>
+                <td>
+                  <Link
+                    to={`/${this.props.match.params.id}/classes/${clas._id}`}
+                  >
+                    {clas.name}
+                  </Link>
+                </td>
+                <td>{clas.class_code}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {renderStudent()}
+      </React.Fragment>
+    );
+  }
 }
 
 export default ClassesTable;
